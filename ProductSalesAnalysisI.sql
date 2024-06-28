77984a4fa6e4b27f3bad6c46cd31318d78bd5ecc
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-06-28 16:53:57 +0530

    Update ProductSalesAnalysisI.sql

--- ProductSalesAnalysisI.sql
+++ ProductSalesAnalysisI.sql
@@ -1,2 +1,4 @@
 
-select Product.product_name,Sales.year,Sales.price from Sales join Product on Sales.product_id=Product.product_id;
+select Product.product_name,Sales.year,Sales.price from Sales 
+join Product on 
+Sales.product_id=Product.product_id;
