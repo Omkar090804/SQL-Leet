bb75811bfcd5c5f30294597c633c10338f69c739
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-06-29 15:48:34 +0530

    Create managerswithaleast5.sql

---
+++ managerswithaleast5.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT e.name
+FROM Employee e
+JOIN (SELECT managerId
+    FROM Employee
+    WHERE managerId IS NOT NULL
+    GROUP BY managerId
+    HAVING COUNT(*) >= 5
+) AS reports ON e.id = reports.managerId;
