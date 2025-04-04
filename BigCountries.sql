3d1fb92267bac2b6cac4fc305ffdcfbe4a4f0f60
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2025-04-04 19:50:08 +0530

    Update BigCountries.sql

--- BigCountries.sql
+++ BigCountries.sql
@@ -1,1 +1,1 @@
-select name,area ,population from World where area >=3000000  or population >= 25000000 ;
+select name, area , population from World where area >=3000000  or population >= 25000000 ;
