efd7395ba7dc17f3c39d4a464368ef743b27c420
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2025-04-04 20:46:19 +0530

    Update CustomerWhoVisitedbutDid NotMakeAnyTransactions.sql

--- CustomerWhoVisitedbutDid NotMakeAnyTransactions.sql
+++ CustomerWhoVisitedbutDid NotMakeAnyTransactions.sql
@@ -2,5 +2,5 @@ SELECT Visits.customer_id, COUNT(Visits.visit_id) AS count_no_trans
 FROM Visits
 LEFT JOIN Transactions ON 
 Visits.visit_id = Transactions.visit_id
-WHERE Transactions.transaction_id IS NULL
-GROUP BY Visits.customer_id;
+WHERE Transactions.transaction_id is NULL
+GROUP BY  Visits.customer_id;
