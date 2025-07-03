6d845a49643daf1faf035b6cbf93ae673cca501b
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2025-07-04 02:42:10 +0530

    Update ImmediateFoodDeliveryII.sql

--- ImmediateFoodDeliveryII.sql
+++ ImmediateFoodDeliveryII.sql
@@ -4,7 +4,7 @@ SELECT
         2
     ) AS immediate_percentage
 FROM Delivery
-WHERE (customer_id, order_date) IN (
+WHERE (customer_id, order_date) IN (           // key , key pair to check each customers order_date and we need minimum so MIN(order_date)
     SELECT customer_id, MIN(order_date)
     FROM Delivery
     GROUP BY customer_id
