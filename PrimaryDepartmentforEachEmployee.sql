94927be9eb8dab031d0ca29301f465d26feb0602
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2025-07-05 02:38:43 +0530

    Create PrimaryDepartmentforEachEmployee.sql

---
+++ PrimaryDepartmentforEachEmployee.sql
@@ -0,0 +1,11 @@
+
+SELECT employee_id, department_id
+FROM Employee
+WHERE primary_flag='Y' or
+    employee_id in
+    (SELECT employee_id
+    FROM Employee
+    Group by employee_id
+    having count(employee_id)=1)
+
+
